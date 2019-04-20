9d044a7a1819d23ac78876d0b32feb1ecc7087e8
alek.frohlich <alek.frohlich@gmail.com>
2019-04-20 16:22:22 -0300

    main.cpp ready for testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,32 @@
 /* libraries */
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <regex>
 
 /* our includes */
 #include "linked_stack.cpp"
 
 void parsexml(const char * filename) {
     structures::LinkedStack<std::string> xmlstack;
+    std::regex tag("<[^>]*>");
     std::ifstream xml;
     xml.open(filename);
     if (xml.is_open()) {
         std::cout << "aberto" << std::endl;
-        std::stringstream ss;
         for(std::string line; std::getline(xml, line);) {
-            for (auto it = line.begin(); it < line.end(); it++) {
-            /* o string::iterator it tem o valor de cada char, *it pra acessa-lo
-
-            Logica de parsing do que esta sendo lido aqui...
-
-            */
-            }
+            std::smatch results;
+            while (regex_search(line, results, tag)) {
+                if (!results.ready())
+                    goto endrsearch; 
+                if (results.str(1)[2] != '/') {
+                    xmlstack.push(results.str(1));
+                } else {
+                    if (results.str(1) != xmlstack.pop())
+                        throw std::out_of_range("unclosed tags!");
+                }
+                line = results.suffix().str();
+        }
+endrsearch: std::cout << "ended regsearch" << std::endl; 
         }
         if (xmlstack.empty()) 
             throw std::out_of_range("unclosed tags!");
